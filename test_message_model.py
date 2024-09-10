e190e9a49faf91c7310e2c50d394234a6255f33f
Agamjot Sodhi <146457410+agamjotsodhi@users.noreply.github.com>
2024-09-10 14:14:06 -0400

    Update test_message_model.py

--- test_message_model.py
+++ test_message_model.py
@@ -4,27 +4,42 @@
 #
 #    python -m unittest test_message_model.py
 
+
 import os
 from unittest import TestCase
-from models import db, User, Message
-from app import app
+from sqlalchemy import exc
 
-# Set an environmental variable to use a different database for tests
+from models import db, User, Message, Follows, Likes
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
-# Create our tables
+
+# Now we can import app
+
+from app import app
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
 db.create_all()
 
-class MessageModelTestCase(TestCase):
-    """Test message model."""
+
+class UserModelTestCase(TestCase):
+    """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
         db.drop_all()
         db.create_all()
 
-        self.uid = 12345
-        u = User.signup("testuser", "test@test.com", "password", None)
+        self.uid = 94566
+        u = User.signup("testing", "testing@test.com", "password", None)
         u.id = self.uid
         db.session.commit()
 
@@ -33,44 +48,49 @@ class MessageModelTestCase(TestCase):
         self.client = app.test_client()
 
     def tearDown(self):
-        """Clean up any fouled transaction."""
+        res = super().tearDown()
         db.session.rollback()
+        return res
 
     def test_message_model(self):
         """Does basic model work?"""
+        
         m = Message(
-            text="A test message",
+            text="a warble",
             user_id=self.uid
         )
 
         db.session.add(m)
         db.session.commit()
 
-        # Message should have content and be linked to a user
+        # User should have 1 message
         self.assertEqual(len(self.u.messages), 1)
-        self.assertEqual(self.u.messages[0].text, "A test message")
+        self.assertEqual(self.u.messages[0].text, "a warble")
 
-    def test_repr(self):
-        """Does the repr method work as expected?"""
-        m = Message(
-            text="A test message",
+    def test_message_likes(self):
+        m1 = Message(
+            text="a warble",
             user_id=self.uid
         )
 
-        db.session.add(m)
-        db.session.commit()
-
-        self.assertEqual(repr(m), f"<Message #{m.id}: A test message, User #{m.user_id}>")
-
-    def test_message_creation(self):
-        """Does the Message model correctly create a new message with a valid input?"""
-        m = Message(
-            text="Another test message",
-            user_id=self.uid
+        m2 = Message(
+            text="a very interesting warble",
+            user_id=self.uid 
         )
 
-        db.session.add(m)
+        u = User.signup("yetanothertest", "t@email.com", "password", None)
+        uid = 888
+        u.id = uid
+        db.session.add_all([m1, m2, u])
         db.session.commit()
 
-        self.assertEqual(m.text, "Another test message")
-        self.assertEqual(m.user_id, self.uid)
+        u.likes.append(m1)
+
+        db.session.commit()
+
+        l = Likes.query.filter(Likes.user_id == uid).all()
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l[0].message_id, m1.id)
+
+
+        
